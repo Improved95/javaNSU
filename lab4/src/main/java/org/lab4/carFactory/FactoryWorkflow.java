c32b4fda9bb34718e6cac71effa8331b2ccea75b
e.davydov <e.davydov@g.nsu.ru>
2024-04-11 14:38:11 +0700

    l4

--- lab4/src/main/java/org/lab4/carFactory/FactoryWorkflow.java
+++ lab4/src/main/java/org/lab4/carFactory/FactoryWorkflow.java
@@ -1,11 +1,17 @@
 package org.lab4.carFactory;
 
 public class FactoryWorkflow {
+    private JFrameObject jFrameObject;
+
+    public FactoryWorkflow() {
+        initial();
+    }
+
     public void execute() {
 
     }
 
     private void initial() {
-
+        jFrameObject = new JFrameObject();
     }
 }
--- lab4/src/main/java/org/lab4/carFactory/JFrameObject.java
+++ lab4/src/main/java/org/lab4/carFactory/JFrameObject.java
@@ -1,5 +1,25 @@
 package org.lab4.carFactory;
 
-public class JFrameObject {
+import javax.swing.*;
+import java.awt.*;
 
+public class JFrameObject {
+    private final int frameWidth = 800;
+    private final int frameHeight = 500;
+
+    private JFrame jFrame;
+
+    public JFrameObject() {
+        this.jFrame = getJFrame();
+    }
+
+    private JFrame getJFrame() {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - frameWidth / 2, dimension.height / 2 - frameHeight / 2, frameWidth, frameHeight);
+        return jFrame;
+    }
 }
