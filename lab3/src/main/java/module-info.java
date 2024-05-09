f35e96f02e4b4807e5e860b56d0c3f71e722ceae
e.davydov <e.davydov@g.nsu.ru>
2024-05-10 00:14:37 +0700

    l5

--- lab3/src/main/java/module-info.java
+++ lab3/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module lab3 {
     requires java.desktop;
     requires javafx.controls;
     requires javafx.media;
-    requires javafx.swingEmpty;
+    requires javafx.swing;
 //    requires javafx.fxml;
 }
