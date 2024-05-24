e14cff5877d494314662dd6bd6321939717d51d2
e.davydov <e.davydov@g.nsu.ru>
2024-05-24 18:38:53 +0700

    начал делать ситему для корректного чтения буфера

--- lab5/communication/src/main/java/org/lab5/communication/communicate/Receiver.java
+++ lab5/communication/src/main/java/org/lab5/communication/communicate/Receiver.java
@@ -1,4 +1,31 @@
 package org.lab5.communication.communicate;
 
+import java.nio.ByteBuffer;
+
 public class Receiver {
+    private final int INT_SIZE = 4;
+
+    private final ByteBuffer messageSizeBuffer = ByteBuffer.allocate(INT_SIZE);
+    private ByteBuffer messageBuffer;
+
+    private int messageSize = 0;
+    private int readBytes = 0;
+
+    public void readReceiveBytes(ByteBuffer byteBuffer) {
+        if (readBytes < INT_SIZE) {
+            byteBuffer.get(0, messageSizeBuffer.array(), 0, INT_SIZE - readBytes);
+            readBytes = messageSizeBuffer.position();
+            if (messageSizeBuffer.position() == INT_SIZE) {
+                messageSizeBuffer.rewind();
+                messageSize = messageSizeBuffer.getInt();
+                messageBuffer = ByteBuffer.allocate(messageSize);
+            }
+        }
+        if (messageSizeBuffer.position() == INT_SIZE) {
+            byteBuffer.get(0, messageBuffer.array(), 0, messageSize);
+            if (messageBuffer.position() == messageSize) {
+
+            }
+        }
+    }
 }
