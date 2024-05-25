39b42e114173930c6ba5e9aaa8933afcdd5fc028
e.davydov <e.davydov@g.nsu.ru>
2024-05-25 16:22:34 +0700

    неудачная попытка сделать сендер

--- lab5/communication/src/main/java/org/lab5/communication/communicate/Sender.java
+++ lab5/communication/src/main/java/org/lab5/communication/communicate/Sender.java
@@ -1,10 +1,58 @@
 package org.lab5.communication.communicate;
 
-public class Sender implements Runnable {
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Sender implements Runnable {
+    private boolean isSending = true;
+
+    private List<SocketAndBufferPair> buffersList = new LinkedList<>();
+
+    public synchronized void addSendDataBuffer(SocketChannel socketChannel, ByteBuffer buffer) {
+        buffersList.add(new SocketAndBufferPair(socketChannel, buffer));
+        notifyAll();
+    }
+
+    public synchronized void waitNewBuffers() throws InterruptedException {
+        while (buffersList.isEmpty()) {
+            wait();
+        }
+    }
+
+    public synchronized void sendingBuffer() throws InterruptedException {
+        while (isSending) {
+            waitNewBuffers();
+
+            Iterator<SocketAndBufferPair> bufferIterator = buffersList.iterator();
+            while (bufferIterator.hasNext()) {
+//                ByteBuffer sendBuffer = bufferIterator.next();
+
+//                if (!sendBuffer.hasRemaining()) {
+//                    bufferIterator.remove();
+//                }
+            }
+        }
+    }
 
     @Override
     public void run() {
+        try {
+            sendingBuffer();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    private class SocketAndBufferPair {
+        public final SocketChannel socketChannel;
+        public final ByteBuffer buffer;
+
+        public SocketAndBufferPair(SocketChannel socketChannel, ByteBuffer buffer) {
+            this.socketChannel = socketChannel;
+            this.buffer = buffer;
+        }
     }
 }
